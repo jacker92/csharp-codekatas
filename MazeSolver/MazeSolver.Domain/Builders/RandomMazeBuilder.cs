69aa73027eb3798030ba050f469dd1bc780dcce1
jacker92 <lahtinen.jaakko@gmail.com>
2021-10-17 21:19:02 +0300

    Refactoring and improvements on RandomMazeBuilder

--- MazeSolver/MazeSolver.Domain/Builders/RandomMazeBuilder.cs
+++ MazeSolver/MazeSolver.Domain/Builders/RandomMazeBuilder.cs
@@ -7,30 +7,35 @@ namespace MazeSolver.Builders
 {
     public class RandomMazeBuilder : IMazeBuilder
     {
+        private readonly Random _random;
+
+        public RandomMazeBuilder()
+        {
+            _random = new Random();
+        }
+
         public IMazeGrid Build(int mazeNumber)
         {
-            var random = new Random();
-            var width = random.Next(50, 100);
-            var tiles = new Tile[width][];
-            for (int i = 0; i < width; i++)
-            {
-                tiles[i] = new Tile[width];
-                for (int j = 0; j < width; j++)
-                {
-                    tiles[i][j] = new Tile { TileType = TileType.Wall, Location = new Point(j, i) };
-                }
-            }
+            var width = _random.Next(50, 100);
+            Tile[][] tiles = InitializeTiles(width);
 
-            var visitedCells = 0;
             var totalCells = width * width;
 
-            var startPosition = new Point(random.Next(0, 5), random.Next(0, 5));
+            var startPosition = new Point(_random.Next(1, 5), _random.Next(1, 5));
             var currentCell = tiles[startPosition.Y][startPosition.X];
             tiles[startPosition.Y][startPosition.X].TileType = TileType.Start;
 
-            var cells = new Stack<Tile>();
+            TraverseTiles(tiles, totalCells, ref currentCell);
 
-            while (visitedCells < totalCells/4)
+            return new MazeGrid(tiles, startPosition, currentCell.Location);
+        }
+
+        private void TraverseTiles(Tile[][] tiles, int totalCells, ref Tile currentCell)
+        {
+            var cells = new Stack<Tile>();
+            var visitedCells = 0;
+
+            while (visitedCells < totalCells / 2.5)
             {
                 // get a list of the neighboring cells with all 4 walls intact
                 var adjacentCells = GetNeighborsWithWalls(currentCell.Location, tiles);
@@ -38,7 +43,7 @@ namespace MazeSolver.Builders
                 if (adjacentCells.Count > 0)
                 {
                     // yes, choose one of them, and knock down the wall between it and the current cell
-                    var randomCellNumber = random.Next(0, adjacentCells.Count());
+                    var randomCellNumber = _random.Next(0, adjacentCells.Count());
 
                     var randomCell = adjacentCells[randomCellNumber];
 
@@ -49,21 +54,40 @@ namespace MazeSolver.Builders
                 }
                 else // No adjacent cells that haven't been visited, go back to the previous cell
                 {
+
                     currentCell = cells.Pop();
                 }
             }
+        }
 
-       
-            return new MazeGrid(tiles, startPosition, currentCell.Location);
+        private static Tile[][] InitializeTiles(int width)
+        {
+            var tiles = new Tile[width][];
+            for (int i = 0; i < width; i++)
+            {
+                tiles[i] = new Tile[width];
+                for (int j = 0; j < width; j++)
+                {
+                    tiles[i][j] = new Tile { TileType = TileType.Wall, Location = new Point(j, i) };
+                }
+            }
+
+            return tiles;
         }
 
         private List<Tile> GetNeighborsWithWalls(Point currentCell, Tile[][] tiles)
         {
-            return MazeHelpers.ProposedLocations(currentCell)
-                .Where(x =>
-                 !MazeHelpers.IsOutOfBounds(new Point(x.X, x.Y), tiles) &&
-                 tiles[x.Y][x.X].TileType == TileType.Wall)
-                .Select(x => tiles[x.Y][x.X]).ToList();
+            var proposed = MazeHelpers.ProposedLocations(currentCell);
+
+            var res = proposed.Where(x =>
+            {
+                return !MazeHelpers.IsOutOfBounds(new Point(x.X, x.Y), tiles) &&
+                tiles[x.Y][x.X].TileType == TileType.Wall;
+            }).ToList();
+
+
+            return res.Count() >= 2 ? res.Select(x => tiles[x.Y][x.X]).ToList() : new List<Tile>();
+
         }
     }
 }
