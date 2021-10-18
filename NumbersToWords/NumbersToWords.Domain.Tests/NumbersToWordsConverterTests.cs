dede0d50faab75dcfd3dfb6ec1a59be2183fc8f6
jacker92 <lahtinen.jaakko@gmail.com>
2021-10-18 19:02:55 +0300

    Added tests for numbers < 21

--- NumbersToWords/NumbersToWords.Domain.Tests/NumbersToWordsConverterTests.cs
+++ NumbersToWords/NumbersToWords.Domain.Tests/NumbersToWordsConverterTests.cs
@@ -20,11 +20,37 @@ namespace NumbersToWords.Domain.Tests
 
         [Theory]
         [InlineData(0, "zero")]
-        public void Convert_ShouldReturnCorrectResult(int value, string convertedValue)
+        [InlineData(1, "one")]
+        [InlineData(2, "two")]
+        [InlineData(3, "three")]
+        [InlineData(4, "four")]
+        [InlineData(5, "five")]
+        [InlineData(6, "six")]
+        [InlineData(7, "seven")]
+        [InlineData(8, "eight")]
+        [InlineData(9, "nine")]
+        public void Convert_ShouldReturnCorrectResult_ForOneDigitNumbers(int value, string convertedValue)
         {
             var result = _numbersToWordsConverter.Convert(value);
             Assert.Equal(convertedValue, result);
+        }
 
+        [Theory]
+        [InlineData(10, "ten")]
+        [InlineData(11, "eleven")]
+        [InlineData(12, "twelve")]
+        [InlineData(13, "thirteen")]
+        [InlineData(14, "fourteen")]
+        [InlineData(15, "fifteen")]
+        [InlineData(16, "sixteen")]
+        [InlineData(17, "seventeen")]
+        [InlineData(18, "eighteen")]
+        [InlineData(19, "nineteen")]
+        [InlineData(20, "twenty")]
+        public void Convert_ShouldReturnCorrectResult_ForTwoDigitNumbers10To20(int value, string convertedValue)
+        {
+            var result = _numbersToWordsConverter.Convert(value);
+            Assert.Equal(convertedValue, result);
         }
     }
 }
--- NumbersToWords/NumbersToWords.Domain/NumbersToWordsConverter.cs
+++ NumbersToWords/NumbersToWords.Domain/NumbersToWordsConverter.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NumbersToWords.Domain
 {
     public class NumbersToWordsConverter
     {
+        private readonly Dictionary<int, string> _dictionary = new Dictionary<int, string>
+        {
+            {0, "zero"},
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {9, "nine"},
+            {10, "ten"},
+            {11, "eleven"},
+            {12, "twelve"},
+            {13, "thirteen"},
+            {14, "fourteen"},
+            {15, "fifteen"},
+            {16, "sixteen"},
+            {17, "seventeen"},
+            {18, "eighteen"},
+            {19, "nineteen"},
+            {20, "twenty"},
+        };
+
         public string Convert(int value)
         {
             if (value < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            if (value < 21)
+            {
+                return _dictionary[value];
             }
             return "zero";
         }
