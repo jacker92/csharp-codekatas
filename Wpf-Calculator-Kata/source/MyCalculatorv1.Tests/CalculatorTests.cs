7018a7c10c254d472358d7b5fdc2b01703b9b4cc
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-23 18:39:44 +0200

    Modified computing logic

--- Wpf-Calculator-Kata/source/MyCalculatorv1.Tests/CalculatorTests.cs
+++ Wpf-Calculator-Kata/source/MyCalculatorv1.Tests/CalculatorTests.cs
@@ -1,13 +1,25 @@
+﻿using System;
 using Xunit;
 
 namespace MyCalculatorv1.Tests
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("5+2", "=7")]
+        [InlineData("5+10", "=15")]
+        [InlineData("0+0", "=0")]
+        [InlineData("-1+1", "=0")]
+        [InlineData("1-2", "=-1")]
+        [InlineData("5-2", "=3")]
+        [InlineData("1/1", "=1")]
+        [InlineData("2*9", "=18")]
+        [InlineData("1/0", "=∞")]
+        public void GetResult_ShouldReturnCorrectResult(string current, string expected)
         {
             var calc = new Calculator();
+            var result = calc.GetResult(current);
+            Assert.Equal(expected, result);
         }
     }
 }
--- Wpf-Calculator-Kata/source/MyCalculatorv1/Calculator.cs
+++ Wpf-Calculator-Kata/source/MyCalculatorv1/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,60 +13,13 @@ namespace MyCalculatorv1
         {
             try
             {
-                int iOp = GetOperationIndex(text);
-
-                string op = text.Substring(iOp, 1);
-                double op1 = Convert.ToDouble(text.Substring(0, iOp));
-                double op2 = Convert.ToDouble(text.Substring(iOp + 1, text.Length - iOp - 1));
-
-                if (op == "+")
-                {
-                    return "=" + (op1 + op2);
-                }
-                else if (op == "-")
-                {
-                    return "=" + (op1 - op2);
-                }
-                else if (op == "*")
-                {
-                    return "=" + (op1 * op2);
-                }
-                else
-                {
-                    return "=" + (op1 / op2);
-                }
+                var res = new DataTable().Compute(text, null);
+                return $"={res}";
             }
             catch (Exception exc)
             {
                 return "Error!";
             }
-        }
-
-        private int GetOperationIndex(string text)
-        {
-            int iOp = 0;
-            if (text.Contains("+"))
-            {
-                iOp = text.IndexOf("+");
-            }
-            else if (text.Contains("-"))
-            {
-                iOp = text.IndexOf("-");
-            }
-            else if (text.Contains("*"))
-            {
-                iOp = text.IndexOf("*");
-            }
-            else if (text.Contains("/"))
-            {
-                iOp = text.IndexOf("/");
-            }
-            else
-            {
-                //error
-            }
-
-            return iOp;
         }
     }
 }
