cfc483e8323fbf859ddc97f3ae88cb8e2824cb84
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-23 18:50:59 +0200

    More tests

--- Wpf-Calculator-Kata/source/MyCalculatorv1.Tests/CalculatorTests.cs
+++ Wpf-Calculator-Kata/source/MyCalculatorv1.Tests/CalculatorTests.cs
@@ -5,6 +5,13 @@ namespace MyCalculatorv1.Tests
 {
     public class CalculatorTests
     {
+        private readonly Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
         [Theory]
         [InlineData("5+2", "5+2=7")]
         [InlineData("5+10", "5+10=15")]
@@ -19,9 +26,16 @@ namespace MyCalculatorv1.Tests
         [InlineData("2*9=17", "2*9=17")]
         public void GetResult_ShouldReturnCorrectResult(string current, string expected)
         {
-            var calc = new Calculator();
-            var result = calc.GetResult(current);
+            var result = _calculator.GetResult(current);
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("**")]
+        public void GetResult_ShouldReturnErrorMessage_WhenExceptionIsThrown(string current)
+        {
+            var result =  _calculator.GetResult(current);
+            Assert.Equal("Error!", result);
         }
     }
 }
