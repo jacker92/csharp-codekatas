2f51b1ac9a64bdbf12360fea4b5e992cdb41e1bc
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-26 10:52:08 +0200

    More equality tests

--- PokerHands/PokerHands.Domain.Tests/PokerHandCompareEqualityTestData.cs
+++ PokerHands/PokerHands.Domain.Tests/PokerHandCompareEqualityTestData.cs
@@ -12,7 +12,7 @@ namespace PokerHands.Domain.Tests
                     new PlayingCard(Suit.Club, 1),
                     new PlayingCard(Suit.Club, 2),
                     new PlayingCard(Suit.Club, 3),
-                    new PlayingCard(Suit.Club, 4),
+                    new PlayingCard(Suit.Club, 7),
                     new PlayingCard(Suit.Club, 5),
                 },
 
@@ -20,8 +20,26 @@ namespace PokerHands.Domain.Tests
                     new PlayingCard(Suit.Club, 1),
                     new PlayingCard(Suit.Club, 2),
                     new PlayingCard(Suit.Club, 3),
+                    new PlayingCard(Suit.Club, 8),
+                    new PlayingCard(Suit.Club, 5),
+                },
+            };
+
+            yield return new object[] {
+                new List<PlayingCard> {
+                    new PlayingCard(Suit.Club, 1),
+                    new PlayingCard(Suit.Club, 2),
+                    new PlayingCard(Suit.Club, 7),
                     new PlayingCard(Suit.Club, 4),
                     new PlayingCard(Suit.Club, 5),
+                },
+
+                   new List<PlayingCard> {
+                    new PlayingCard(Suit.Heart, 1),
+                    new PlayingCard(Suit.Heart, 2),
+                    new PlayingCard(Suit.Heart, 8),
+                    new PlayingCard(Suit.Heart, 4),
+                    new PlayingCard(Suit.Heart, 5),
                 },
             };
         }
