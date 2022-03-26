43b0cd8e86f0b2a31fd17ea0e10c2ee9f479df63
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-26 18:16:22 +0200

    Rest of the rules

--- RockPaperScissors/RockPaperScissors.Domain.Tests/RockPaperScissorsEngineTests.cs
+++ RockPaperScissors/RockPaperScissors.Domain.Tests/RockPaperScissorsEngineTests.cs
@@ -9,6 +9,11 @@ namespace RockPaperScissors.Domain.Tests
         [InlineData(Gesture.Rock, Gesture.Scissors, Gesture.Rock)]
         [InlineData(Gesture.Scissors, Gesture.Rock, Gesture.Rock)]
         [InlineData(Gesture.Scissors, Gesture.Paper, Gesture.Scissors)]
+        [InlineData(Gesture.Scissors, Gesture.Scissors, Gesture.Scissors)]
+        [InlineData(Gesture.Paper, Gesture.Scissors, Gesture.Scissors)]
+        [InlineData(Gesture.Paper, Gesture.Paper, Gesture.Paper)]
+        [InlineData(Gesture.Paper, Gesture.Rock, Gesture.Paper)]
+        [InlineData(Gesture.Rock, Gesture.Paper, Gesture.Paper)]
         public void CalculateWinner_ShouldCalculateWinnerCorrectly(Gesture player1, Gesture player2, Gesture expectedGestureToWin)
         {
             var rockPaperScissorsEngine = new RockPaperScissorsEngine();
--- RockPaperScissors/RockPaperScissors.Domain/RockPaperScissorsEngine.cs
+++ RockPaperScissors/RockPaperScissors.Domain/RockPaperScissorsEngine.cs
@@ -7,25 +7,40 @@
             switch (player1)
             {
                 case Gesture.Rock:
-                    if (player2 == Gesture.Paper)
-                    {
-                        return Gesture.Paper;
-                    }
-                    return Gesture.Rock;
+                    return HandleRock(player2);
                 case Gesture.Paper:
-                    if(player2 == Gesture.Scissors)
-                    {
-                        return Gesture.Scissors;
-                    }
-                    break;
+                    return HandlePaper(player2);
                 case Gesture.Scissors:
-                    if (player2 == Gesture.Rock)
-                    {
-                        return Gesture.Rock;
-                    }
-                    return Gesture.Scissors;
+                    return HandleScissors(player2);
                 default:
                     break;
+            }
+            return Gesture.Rock;
+        }
+
+        private static Gesture HandleScissors(Gesture player2)
+        {
+            if (player2 == Gesture.Rock)
+            {
+                return Gesture.Rock;
+            }
+            return Gesture.Scissors;
+        }
+
+        private static Gesture HandlePaper(Gesture player2)
+        {
+            if (player2 == Gesture.Scissors)
+            {
+                return Gesture.Scissors;
+            }
+            return Gesture.Paper;
+        }
+
+        private static Gesture HandleRock(Gesture player2)
+        {
+            if (player2 == Gesture.Paper)
+            {
+                return Gesture.Paper;
             }
             return Gesture.Rock;
         }
