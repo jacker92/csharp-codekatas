b02b64cf4f2d68c681d87348acfac6051d66331f
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-27 12:06:19 +0300

    Added country demoninations as constants

--- ChangeMaker/ChangeMaker.Domain.Tests/VendingMachineTests.cs
+++ ChangeMaker/ChangeMaker.Domain.Tests/VendingMachineTests.cs
@@ -5,15 +5,6 @@ namespace ChangeMaker.Domain.Tests
 {
     public class VendingMachineTests
     {
-        /*
-         * var coinDenominations = [1,5,10,25]; // coin values converted to whole numbers
-            var machine = new VendingMachine(coinDenominations);
-            var purchaseAmount = 1.25; // amount the item cost
-            var tenderAmount = 2.00; // amount the user input for the purchase
-            var change = machine.CalculateChange(purchaseAmount, tenderAmount);
-         * 
-         */
-
         [Fact]
         public void ShouldThrowArgumentNullException_IfNullCoinDenominationsIsGiven()
         {
@@ -25,7 +16,7 @@ namespace ChangeMaker.Domain.Tests
         [InlineData(1.97, 2.00, 1, 1, 1)]
         public void CalculateChange_ShouldWork(double purchaseAmount, double tenderAmount, double change1, double change2, double change3 )
         {
-            var vendingMachine = new VendingMachine(new int[] { 1, 5, 10, 25 });
+            var vendingMachine = new VendingMachine(CountryDemoninations.USDollar);
             var result = vendingMachine.CalculateChange(purchaseAmount, tenderAmount);
 
             Assert.Equal(3, result.Length);
---
+++ ChangeMaker/ChangeMaker.Domain/CountryDemoninations.cs
@@ -0,0 +1,9 @@
+﻿namespace ChangeMaker.Domain
+{
+    public static class CountryDemoninations
+    {
+        public static int[] BritishPound = new int[] { 1, 2, 5, 10, 20, 50 };
+        public static int[] USDollar = new int[] { 1, 5, 10, 25 };
+        public static int[] NorwegianKrone = new int[] { 1, 5, 10, 20 };
+    }
+}
