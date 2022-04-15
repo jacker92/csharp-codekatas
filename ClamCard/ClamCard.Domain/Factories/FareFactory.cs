d0286e18e38c8296a91a0b84f769d4d2ef9597d0
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-15 20:19:43 +0300

    FareFactory to dynamic factory

--- ClamCard/ClamCard.Domain/Factories/FareFactory.cs
+++ ClamCard/ClamCard.Domain/Factories/FareFactory.cs
@@ -1,18 +1,27 @@
 ﻿using ClamCard.Domain.Models;
 using ClamCard.Domain.Models.Fares;
+using System.Collections.Immutable;
 
 namespace ClamCard.Domain.Factories
 {
     public class FareFactory
     {
+        private readonly IReadOnlyDictionary<Zone, IZoneFare> _zoneFareProviders;
+
+        public FareFactory()
+        {
+            var zoneFareType = typeof(IZoneFare);
+            _zoneFareProviders = zoneFareType.Assembly
+                .ExportedTypes
+                .Where(x => zoneFareType.IsAssignableFrom(x) && !x.IsAbstract && !x.IsInterface)
+                .Select(x => Activator.CreateInstance(x))
+                .Cast<IZoneFare>()
+                .ToImmutableDictionary(x => x.Zone, x => x);
+        }
+
         public IZoneFare GetFareFor(Zone zone)
         {
-            return zone switch
-            {
-                Zone.A => new ZoneAFare(),
-                Zone.B => new ZoneBFare(),
-                _ => throw new Exception(),
-            };
+            return _zoneFareProviders[zone];
         }
     }
 }
