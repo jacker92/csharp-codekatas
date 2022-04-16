579763f25aafd9fc2c6fbd58ab735d794ccbe955
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-16 18:53:38 +0300

    Added CreatedColumn migration

--- SocialNetwork/SocialNetwork.Infrastructure/Migrations/20220416155314_CreatedColumn.Designer.cs
+++ SocialNetwork/SocialNetwork.Infrastructure/Migrations/20220416155314_CreatedColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialNetwork.Infrastructure;
 
@@ -10,9 +11,10 @@ using SocialNetwork.Infrastructure;
 namespace SocialNetwork.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220416155314_CreatedColumn")]
+    partial class CreatedColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.4");
