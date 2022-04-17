74a929da9566e31eabe33023af2fe5cb227ab648
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-17 10:53:16 +0300

    Removed mapping logic

--- SocialNetwork/SocialNetwork.Application/Mappings/MapperFactory.cs
+++ SocialNetwork/SocialNetwork.Application/Mappings/MapperFactory.cs
@@ -23,7 +23,6 @@ namespace SocialNetwork.Application.Mappings
 
         private static void Users(IMapperConfigurationExpression cfg)
         {
-            cfg.CreateMap<UpdateUserRequest, User>().ForMember(x => x.Subscriptions, opt => opt.Ignore());
             cfg.CreateMap<User, CreateUserResponse>().ForMember(x => x.Subscriptions, opt => opt.MapFrom(src => src.Subscriptions.Select(x => x.Id)));
             cfg.CreateMap<User, GetUserResponse>();
             cfg.CreateMap<User, UpdateUserResponse>();
