38b0c2c05b2ba9feffea86ed08f68d18239eff1f
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-17 20:28:35 +0300

    Refactor

--- BingoKata/BingoKata.Domain/BingoCardChecker.cs
+++ BingoKata/BingoKata.Domain/BingoCardChecker.cs
@@ -4,6 +4,45 @@
     {
         public bool HasBingo(List<int> list, BingoCard bingoCard)
         {
+            if (HasVerticalBingo(list, bingoCard))
+            {
+                return true;
+            }
+
+            if (HasHorizontalBingo(list, bingoCard))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool HasHorizontalBingo(List<int> list, BingoCard bingoCard)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                var hasHorizonatlBingo = true;
+                for (int x = 0; x < 5; x++)
+                {
+                    var current = bingoCard.SpaceRows[x, y];
+
+                    if (current.Value.HasValue && !list.Contains(current.Value.Value))
+                    {
+                        hasHorizonatlBingo = false;
+                        break;
+                    }
+                }
+
+                if (hasHorizonatlBingo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool HasVerticalBingo(List<int> list, BingoCard bingoCard)
+        {
             for (int x = 0; x < 5; x++)
             {
                 var isBingo = true;
@@ -24,30 +63,6 @@
                 }
             }
 
-   
-            for (int y = 0; y < 5; y++)
-            {
-                var hasHorizonatlBingo = true;
-                for (int x = 0; x < 5; x++)
-                {
-                    var current = bingoCard.SpaceRows[x, y];
-
-                    if (current.Value.HasValue && !list.Contains(current.Value.Value))
-                    {
-                        hasHorizonatlBingo = false;
-                        break;
-                    }
-
-
-
-                }
-
-                if (hasHorizonatlBingo)
-                {
-                    return true;
-                }
-            }
-            
             return false;
         }
     }
